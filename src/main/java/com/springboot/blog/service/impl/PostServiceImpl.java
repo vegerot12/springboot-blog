ce96bf1a8cb5d54e6987bdc875c35217136b3846
nandhini <nandhinirajavell@gmail.com>
2024-01-03 15:49:36 +0530

    sorting support

--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -9,6 +9,7 @@ import com.springboot.blog.service.PostService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -37,10 +38,12 @@ private PostRepository postRepository;
     }
 
     @Override
-    public PostResponse getAllPosts(int pageNo, int pageSize) {
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy,  String sortDir) {
 
+        // sort dir
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ?Sort.by(sortBy).ascending() : Sort.by(sortBy).descending() ;
         // create pageable instance
-        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Pageable pageable = PageRequest.of(pageNo,pageSize, sort); // .descending()
 
         // pass pageable instance to findaLl
         Page<Post> posts =  postRepository.findAll(pageable);
