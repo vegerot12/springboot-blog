4dc3bc12ad124f091ac22b2fa458d7660bf79a8b
nandhini <nandhinirajavell@gmail.com>
2024-01-30 18:37:18 +0530

    versioning api - uri path

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -2,6 +2,7 @@ package com.springboot.blog.controller;
 
 import com.springboot.blog.entity.Post;
 import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostDtoV2;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
 import com.springboot.blog.utils.AppConstants;
@@ -15,10 +16,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping()
 @Tag(
     name="CRUD REST API for Post resource"
 )
@@ -41,7 +43,7 @@ public class PostController {
         name = "Bear Authentication"
 )
 @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping
+    @PostMapping("/api/v1/posts")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         System.out.println("In post mapping of controller");
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
@@ -57,7 +59,7 @@ public class PostController {
         responseCode = "200",
         description = "Http status 200 SUCCESS"
 )
-    @GetMapping
+    @GetMapping("/api/v1/posts")
     public PostResponse getAllPosts(
             @RequestParam(value="pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(value="pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
@@ -78,11 +80,28 @@ public class PostController {
         responseCode = "200",
         description = "Http status 200 SUCCESS"
 )
-    @GetMapping("/{id}")
+    @GetMapping("/api/v1/posts/{id}")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id){
         return ResponseEntity.ok(postService.getPostById(id));
     }
+    // supports tags
+    @GetMapping("/api/v2/posts/{id}")
+    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name = "id") long id){
+        PostDto postDto = postService.getPostById(id);
+        PostDtoV2 postDtoV2 = new PostDtoV2();
+        postDtoV2.setId(postDto.getId());
+        postDtoV2.setTitle(postDto.getTitle());
+        postDtoV2.setDescription(postDto.getDescription());
+        postDtoV2.setContent(postDto.getContent());
+        List<String> tags = new ArrayList<>();
+        tags.add("Java");
+        tags.add("JS");
 
+postDtoV2.setTags(tags);
+
+
+        return ResponseEntity.ok(postDtoV2);
+    }
     // update post by id
     @Operation(
             summary = "Update post by id REST Api",
@@ -96,7 +115,7 @@ public class PostController {
     @SecurityRequirement(
             name = "Bear Authentication"
     )
-    @PutMapping("/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto , @PathVariable(name = "id") long id){
         return ResponseEntity.ok(postService.updatePost(postDto,id));
     }
@@ -115,7 +134,7 @@ public class PostController {
     )
     @PreAuthorize("hasRole('ADMIN')")
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/v1/posts/{id}")
     public ResponseEntity<String> deletePost( @PathVariable(name = "id") long id){
         postService.deletePost(id);
         return new ResponseEntity<>("Post deleted successfully", HttpStatus.OK);
@@ -130,7 +149,7 @@ public class PostController {
             responseCode = "200",
             description = "Http status 200 SUCCESS"
     )
-    @GetMapping("category/{id}")
+    @GetMapping("/api/v1/posts/category/{id}")
     public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable("id") Long categoryId){
         List<PostDto> postDtos = postService.getPostByCategory(categoryId);
         return ResponseEntity.ok(postDtos);
